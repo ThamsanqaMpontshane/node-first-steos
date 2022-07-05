d4123412758b5b85b8e1e446a4227971a05c76bf
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-07-05 12:30:49 +0200

    Third Commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,5 +1,15 @@
 import chalk from "chalk";
+import figlet from "figlet";
 import greet from "./greet.js";
+
+figlet("Hello Xola", (err, data) => {
+    if (err) {
+        console.log(chalk.red(err));
+    } else {
+        console.log(chalk.green(data));
+    }
+}
+);
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage);
